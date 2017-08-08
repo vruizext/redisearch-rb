84ebeb4320986aae009d871898c0a088dbb652e3
vruizext <vruizext@gmail.com>
2017-08-09 00:54:14 +0200

    Easier test config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,17 +10,19 @@ require './lib/redisearch-rb'
 Dotenv.load('./.env')
 
 class RedisTestServer
-  DEFAULT_REDIS_PATH = ''#'redis-server'
+  DEFAULT_REDIS_PATH = 'redis-server'
 
-  attr_reader :process
+  attr_reader :process, :url
 
-  def initialize
+  def initialize(default_port = nil)
     @command = [ENV['REDIS_SERVER_PATH'] || DEFAULT_REDIS_PATH]
+    @port = ENV['REDIS_PORT'] || default_port
   end
 
   def start
     build_args
     @process = Subprocess.popen(@command)
+    @url = "redis://127.0.0.1:#{@port}"
   rescue => error
     Logger.new(STDERR).error(error.message)
     @process = nil
@@ -36,7 +38,7 @@ class RedisTestServer
 
   def build_args
     if ENV['REDIS_CONF_PATH'].to_s.empty?
-      @command << "--port #{ENV['REDIS_PORT']}" unless ENV['REDIS_PORT'].to_s.empty?
+      @command << "--port #{@port}" unless @port.to_s.empty?
       @command << "--loadmodule #{ENV['REDIS_MODULE_PATH']}" unless ENV['REDIS_MODULE_PATH'].to_s.empty?
     else
       @command << ENV['REDIS_CONF_PATH']
